301059dc4bf258252d8cb58367b260f4de4d62da
Nubia Barcellar Moreira <nubia.moreira@zup.com.br>
2025-01-29 19:19:31 -0300

    feat: add model client

---
+++ src/main/java/com/e_commerce/models/ClientModel.java
@@ -0,0 +1,40 @@
+package com.e_commerce.models;
+
+public class ClientModel {
+    private String name;
+    private String CPF;
+    private String email;
+
+    public ClientModel() {
+    }
+
+    public ClientModel(String name, String CPF, String email){
+        this.name = name;
+        this.CPF = CPF;
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getCPF() {
+        return CPF;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setCPF(String CPF) {
+        this.CPF = CPF;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
