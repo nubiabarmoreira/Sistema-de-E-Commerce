72c7e4c521d1ecbf40992704b4a28a069a7d49f2
Nubia Barcellar Moreira <nubia.moreira@zup.com.br>
2025-01-30 15:53:38 -0300

    feat: add DTO product

---
+++ src/main/java/com/e_commerce/dtos/ProductDTO.java
@@ -0,0 +1,39 @@
+package com.e_commerce.dtos;
+
+public class ProductDTO {
+    private String name;
+    private Double price;
+    private int quantity;
+
+    public ProductDTO() {}
+
+    public ProductDTO(String name, Double price, int quantity) {
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+}
