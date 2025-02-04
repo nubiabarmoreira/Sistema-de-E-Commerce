6ec7fe856c2701ce70172338fc9a4f66244629a7
Nubia Barcellar Moreira <nubia.moreira@zup.com.br>
2025-02-04 16:42:37 -0300

    feat: add id product model

--- src/main/java/com/e_commerce/models/ClientModel.java
+++ src/main/java/com/e_commerce/models/ClientModel.java
@@ -1,9 +1,15 @@
 package com.e_commerce.models;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
 @Entity
 public class ClientModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
     private String name;
     private String CPF;
     private String email;
