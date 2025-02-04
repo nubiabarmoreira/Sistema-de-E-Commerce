014885b471252a25fb4a88c3cc36860f8b3ecdad
Nubia Barcellar Moreira <nubia.moreira@zup.com.br>
2025-02-04 11:21:54 -0300

    feat: add annotations class product DTO

--- src/main/java/com/e_commerce/controllers/ProductController.java
+++ src/main/java/com/e_commerce/controllers/ProductController.java
@@ -2,6 +2,7 @@ package com.e_commerce.controllers;
 
 import com.e_commerce.dtos.ProductDTO;
 import com.e_commerce.services.ProductService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,7 +17,7 @@ public class ProductController {
     private ProductService productService;
 
     @PostMapping
-    public ResponseEntity<ProductDTO> createProduct (@RequestBody ProductDTO productRequest){
+    public ResponseEntity<ProductDTO> createProduct (@Valid @RequestBody ProductDTO productRequest){
         ProductDTO productResponse = productService.createProduct(productRequest.getName(), productRequest.getPrice(), productRequest.getQuantity());
         return ResponseEntity.status(HttpStatus.CREATED).body(productResponse);
     }
--- src/main/java/com/e_commerce/dtos/ProductDTO.java
+++ src/main/java/com/e_commerce/dtos/ProductDTO.java
@@ -1,8 +1,20 @@
 package com.e_commerce.dtos;
 
+import jakarta.persistence.Column;
+import jakarta.validation.constraints.*;
+
 public class ProductDTO {
+    @NotBlank(message = "O nome do produto deve ser informado.")
+    @Column(unique = true)
+    @Size(min = 2, max = 100, message = "O nome do produto deve ter entre 2 e 100 caracteres.")
     private String name;
+
+    @NotNull(message = "O preço do produto deve ser informado.")
+    @Positive(message = "O preço do produto deve ser maior do que zero.")
     private Double price;
+
+    @NotNull(message = "A quantidade do produto deve ser informada.")
+    @PositiveOrZero(message = "A quantidade do produto deve ser maior ou igual a zero.")
     private int quantity;
 
     public ProductDTO() {}
