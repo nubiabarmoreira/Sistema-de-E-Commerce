24d6e349acab6a8b014dc506bfb5cb49923f7cbb
Nubia Barcellar Moreira <nubia.moreira@zup.com.br>
2025-02-05 11:12:21 -0300

    fix: change jpa type

--- src/main/java/com/e_commerce/repositories/DatabaseClientRepository.java
+++ src/main/java/com/e_commerce/repositories/DatabaseClientRepository.java
@@ -6,6 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-public interface DatabaseClientRepository extends JpaRepository<ClientModel, Long> {
+public interface DatabaseClientRepository extends JpaRepository<ClientModel, String> {
     Optional<ClientModel> findClientByCpf(String cpf);
 }
--- src/main/java/com/e_commerce/repositories/DatabaseProductRepository.java
+++ src/main/java/com/e_commerce/repositories/DatabaseProductRepository.java
@@ -3,5 +3,5 @@ package com.e_commerce.repositories;
 import com.e_commerce.models.ProductModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface DatabaseProductRepository extends JpaRepository<ProductModel, Long> {
+public interface DatabaseProductRepository extends JpaRepository<ProductModel, String> {
 }
