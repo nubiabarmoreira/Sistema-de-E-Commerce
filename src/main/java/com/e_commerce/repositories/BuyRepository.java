e482aaa29861db53b890ccb54f0374b3176d04ca
Nubia Barcellar Moreira <nubia.moreira@zup.com.br>
2025-02-06 13:24:38 -0300

    chore: delete files BuyRepository

--- src/main/java/com/e_commerce/repositories/BuyRepository.java
+++
@@ -1,4 +0,0 @@
-package com.e_commerce.repositories;
-
-public interface BuyRepository {
-}
--- src/main/java/com/e_commerce/repositories/DatabaseBuyRepository.java
+++
@@ -1,7 +0,0 @@
-package com.e_commerce.repositories;
-
-import com.e_commerce.models.BuyModel;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DatabaseBuyRepository extends JpaRepository<BuyModel, Long> {
-}
