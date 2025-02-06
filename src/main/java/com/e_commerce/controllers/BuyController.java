c3853dc946ccff14b3aa3d59461124a5d0c1e831
Nubia Barcellar Moreira <nubia.moreira@zup.com.br>
2025-02-06 13:26:53 -0300

    fix: fix validations and refactor makePurchase method

--- src/main/java/com/e_commerce/controllers/BuyController.java
+++ src/main/java/com/e_commerce/controllers/BuyController.java
@@ -1,15 +1,15 @@
 package com.e_commerce.controllers;
 
 import com.e_commerce.dtos.BuyDTO;
+import com.e_commerce.dtos.ProductDTO;
 import com.e_commerce.services.BuyService;
-import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/compras")
@@ -18,8 +18,7 @@ public class BuyController {
     BuyService buyService;
 
     @PostMapping
-    public ResponseEntity<BuyDTO> makePurchase (@Valid @RequestBody BuyDTO buyRequest){
-       buyService.makePurchase(buyRequest);
-        return ResponseEntity.status(HttpStatus.OK).body(buyRequest);
+    public void makePurchase (@RequestBody BuyDTO buyRequest) {
+        buyService.makePurchase(buyRequest);
     }
 }
--- src/main/java/com/e_commerce/services/BuyServiceImpl.java
+++ src/main/java/com/e_commerce/services/BuyServiceImpl.java
@@ -4,39 +4,63 @@ import com.e_commerce.dtos.BuyDTO;
 import com.e_commerce.dtos.ClientDTO;
 import com.e_commerce.dtos.ProductDTO;
 import com.e_commerce.models.ProductModel;
-import com.e_commerce.repositories.BuyRepository;
 import com.e_commerce.repositories.ClientRepository;
 import com.e_commerce.repositories.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import static org.apache.logging.log4j.util.Strings.isEmpty;
-
 @Service
 public class BuyServiceImpl implements BuyService {
     @Autowired
-    BuyRepository buyRepository;
     ClientRepository clientRepository;
+    @Autowired
     ProductRepository productRepository;
 
     @Override
     public void makePurchase(BuyDTO buyRequest) {
+//        checkIfClientExists(buyRequest);
+//        checkIfProductExistsAndCheckQuantityOfProduct(buyRequest);
+
         ClientDTO clientToBuy = clientRepository.findClientByCPF(buyRequest.getCPF());
-        if(clientToBuy == null){
+        if (clientToBuy == null){
             throw new RuntimeException("Cliente com CPF " + clientToBuy.getCPF() + " não encontrado.");
         }
 
-        for (ProductDTO productDTO : buyRequest.getProductsList()){
-            ProductModel productModel = productRepository.findProductByName(productDTO.getName());
-                  //  .orElseThrow(() -> new RuntimeException("Produto não encontrado: " + productDTO.getName()));
-
-            if(productModel.getQuantity() <= 0){
-                throw new RuntimeException("Produto sem estoque: " + productDTO.getName());
+        for (ProductDTO productDTO : buyRequest.getProductsList()) {
+            ProductDTO product = productRepository.findProductByName(productDTO.getName());
+            if (product == null) {
+                throw new RuntimeException("Produto não encontrado: " + productDTO.getName());
             }
 
-            productModel.setQuantity(productModel.getQuantity() - buyRequest.getQuantityToBuy());
-            ProductDTO productToBuy = new ProductDTO(productModel.getName(), productModel.getPrice(), productModel.getQuantity());
+            if (product.getQuantity() <= 0){
+                throw new RuntimeException("Produto sem estoque: " + product.getName());
+            }
+
+            product.setQuantity(product.getQuantity() - buyRequest.getQuantityToBuy());
+            ProductDTO productToBuy = new ProductDTO(product.getName(), product.getPrice(), product.getQuantity());
             productRepository.saveProduct(productToBuy);
         }
+
+//    private void checkIfClientExists(BuyDTO buyRequest) {
+//        ClientDTO clientToBuy = clientRepository.findClientByCPF(buyRequest.getCPF());
+//        if(clientToBuy == null){
+//            throw new RuntimeException("Cliente com CPF " + clientToBuy.getCPF() + " não encontrado.");
+//        }
+//    }
+
+//    private void checkIfProductExistsAndCheckQuantityOfProduct(BuyDTO buyRequest) {
+//        for (ProductDTO productDTO : buyRequest.getProductsList()) {
+//            ProductDTO product = productRepository.findProductByName(productDTO.getName());
+//            if (product == null) {
+//                throw new RuntimeException("Produto não encontrado: " + productDTO.getName());
+//            }
+//
+//        if (product.getQuantity() <= 0){
+//            throw new RuntimeException("Produto sem estoque: " + product.getName());
+//        }
+//
+//        product.setQuantity(product.getQuantity() - buyRequest.getQuantityToBuy());
+//        ProductDTO productToBuy = new ProductDTO(product.getName(), product.getPrice(), product.getQuantity());
+//        productRepository.saveProduct(productToBuy);
     }
 }
